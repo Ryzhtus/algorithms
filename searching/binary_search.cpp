4234ea10ca91b379e22b3dd013f3ef0f73ed2231
Arseniy Shkunkov <shkunkov.a@yandex.ru>
2022-05-10 14:56:31 +0300

    Code style changes in searching algorithms

--- searching/binary_search.cpp
+++ searching/binary_search.cpp
@@ -2,22 +2,19 @@
 #include <vector>
 
 // return the index of the array if it contains the value, else return -1  
-int binarySearch(std::vector<int> array, int left, int right, int value) {
+int BinarySearch(std::vector<int> array, int left, int right, int value) {
     if (left <= right) {
         int middle = left + (right - left) / 2;
 
         if (array[middle] == value) {
             return middle;
-        } 
-        else if (array[middle] > value) 
-        {
-            return binarySearch(array, left, middle - 1, value);
-        } 
-        else 
-        {
-            return binarySearch(array, middle + 1, right, value);
+        } else if (array[middle] > value) {
+            return BinarySearch(array, left, middle - 1, value);
+        } else {
+            return BinarySearch(array, middle + 1, right, value);
         }
     }
+
     return -1;
 }
 
@@ -25,7 +22,7 @@ int main() {
     std::vector<int> values = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int search_value = 4;
 
-    std::cout << binarySearch(values, 0, values.size(), search_value) << std::endl;
+    std::cout << BinarySearch(values, 0, values.size(), search_value) << std::endl;
 
     return 0;
 }
--- searching/linear_search.cpp
+++ searching/linear_search.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 // return the index of the array if it contains the value, else return -1  
-int linearSearch(std::vector<int> array, int value) {
+int LinearSearch(std::vector<int> array, int value) {
     for (int i = 0; i < array.size(); i++) {
         if (array[i] == value) {
             return i;
@@ -15,7 +15,7 @@ int main() {
     std::vector<int> values = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int search_value = 4;
 
-    std::cout << linearSearch(values, search_value) << std::endl;
+    std::cout << LinearSearch(values, search_value) << std::endl;
 
     return 0;
 }
